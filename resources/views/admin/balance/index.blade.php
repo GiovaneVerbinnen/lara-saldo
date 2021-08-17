0d4d9e0add8a915e79e60479293cd95b4d18f6a6
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-08-17 10:33:22 -0300

    aula 16 terminada

---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title', 'Saldo')
+
+@section('content_header')
+<h1>Saldo</h1>
+<ol class="breadcrumb">
+    <li><a href="">Dashboard</a></li>
+    <li><a href="">Saldo</a></li>
+</ol>
+@stop
+
+@section('content')
+<div class="box">
+    <div class="box-header">
+        <a class="btn btn-primary"><i class="fa fa-arrow-circle-up"></i> Recarregar</a>
+        <a class="btn btn-warning"><i class="fa fa-arrow-circle-down"></i> Sacar</a>
+    </div>
+    <div class="box-body">
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-cash"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
+@stop
