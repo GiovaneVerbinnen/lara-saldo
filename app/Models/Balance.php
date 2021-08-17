cf0eaa4cb9fc99ec6c53640bbc83e75021dd3999
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-08-17 14:30:29 -0300

    aula 21 terminada

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Balance extends Model
 {
@@ -10,10 +11,19 @@ class Balance extends Model
 
     public function deposit(float $value): array
     {
+        $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.', '');
         $deposit = $this->save();
 
-        if ($deposit)
+        $historic = auth()->user()->historics()->create([
+            'type'                  => 'I',
+            'amount'                => $value,
+            'total_before'          => $totalBefore,
+            'total_after'           => $this->amount,
+            'date'                  => 'Ymd',
+        ]);
+
+        if ($deposit && $historic)
             return [
                 'sucess' => true,
                 'message' => 'Sucesso ao recarregar'
